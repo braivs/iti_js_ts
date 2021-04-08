077823b61208713255c4f52b9ded7efcf9b97b38
briws <briws@pm.me>
2021-04-09 00:10:59 +0300

    06 Destructuring - part

---
+++ src/07-destructuring-assignment/destructuring.test.ts
@@ -0,0 +1,27 @@
+test("", () => {
+  let props = {
+    name: 'Dimych',
+    age: 32,
+    lessons: [{title: '1'}, {title: '2'}],
+    address: {
+      street : {
+        title: 'Nezavisimosti street'
+      }
+    }
+  }
+
+  //const age = props.age;
+  //const lessons = props.lessons;
+  const {age, lessons} = props;
+  const {title} = props.address.street;
+
+  const a = props.age;
+  const l = props.lessons;
+
+  expect(age).toBe(32);
+  expect(lessons.length).toBe(2);
+
+  expect(a).toBe(32);
+  expect(l.length).toBe(2);
+  expect(title).toBe('Nezavisimosti street');
+})
