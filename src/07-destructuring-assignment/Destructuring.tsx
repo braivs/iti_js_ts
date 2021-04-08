dd8917638efe201bb47cf80bf37af0c465dfe0e4
briws <briws@pm.me>
2021-04-09 01:13:27 +0300

    06 Destructuring - end of dest. objects

--- src/07-destructuring-assignment/Destructuring.tsx
+++ src/07-destructuring-assignment/Destructuring.tsx
@@ -13,13 +13,19 @@ export type ManType = {
 type PropsType = {
   title: string
   man: ManType
+  food: Array<string>
+  car: {model: string}
 }
 
-export const ManComponent: React.FC<PropsType> = ({title, man}) => {
+export const ManComponent: React.FC<PropsType> = ({title, man, ...props}) => {
+  // const {title, man, ...restProps} = props;
 
   return <div>
     <h1>{title}</h1>
     <hr/>
+    <div>
+      {props.car.model}
+    </div>
     <div>
       {man.name}
     </div>
